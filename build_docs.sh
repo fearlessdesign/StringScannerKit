1e92ae0c1618318c478c1756eef5e194c947e1d0
Electric Sidecar <github@electricsidecar.app>
2024-06-06 16:50:56 -0700

    Fix root site url.

--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-swift package --allow-writing-to-directory docs generate-documentation --target ScannerExtensions --disable-indexing --transform-for-static-hosting --hosting-base-path ScannerExtensions --output-path docs
+swift package --allow-writing-to-directory docs generate-documentation --target ScannerExtensions --disable-indexing --transform-for-static-hosting --hosting-base-path StringScannerKit --output-path docs
 swift package --disable-sandbox preview-documentation --target ScannerExtensions
